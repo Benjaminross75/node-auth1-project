0a466527edd8d1cc3afae445507258111dd977bf
Ben Ross <mrbenross@gmail.com>
2024-01-15 09:53:01 -0600

    final-commit

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -6,13 +6,13 @@ const {restricted} = require('../auth/auth-middleware')
 const User = require('./users-model')
 
 router.get('/', restricted, async (req, res, next) =>{
-// try{
-//   const users = await User.find()
-//   res.json(users)
-// }
-// catch(err){
-//   next(err)
-// }
+try{
+  const users = await User.find()
+  res.json(users)
+}
+catch(err){
+  next(err)
+}
 })
 
 /**
